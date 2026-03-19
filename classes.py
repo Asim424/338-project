d39871f610cce950f134a682444ba5725d675ade
Asim424 <asimhamdan2005@gmail.com>
2026-03-19 09:23:01 -0600

    fixed priority queue bugs, added dijsktra's algorithm

--- classes.py
+++ classes.py
@@ -190,16 +190,23 @@ class Room:
 class priorityQ:
     def __init__(self):
         self.body = []
-    def enqueue(self,item, priority : int):
+
+    def enqueue(self, item, priority):
+        inserted = False
+
         for i in range(len(self.body)):
-            if(priority >= self.body[i][0]):
-                if self.body is not None:
-                    self.body.insert(i, [priority,item])
-                else:
-                    self.body =[[priority,item]]
+            if priority < self.body[i][0]:  # smaller = higher priority
+                self.body.insert(i, [priority, item])
+                inserted = True
+                break
+
+        if not inserted:
+            self.body.append([priority, item])
+
     def dequeue(self):
-        output = self.body.pop(0)
-        return output
+        if not self.body:
+            return None
+        return self.body.pop(0)  # returns [priority, item]
 
 class priority_service:
     def __init__(self):
@@ -219,22 +226,91 @@ class Building:
 
         self.location = location    # (lat, lon) or grid coords
 
-        self.rooms = priorityQ() # room_id -> Room  
+        self.rooms = priorityQ() # room_id -> Room
+
+        self.vertices = Queue()  
     
     def add_room(self, item: Room, id : str):
         self.rooms.enqueue(item, id) #guarantees that rooms are sorted by id, to make lookup quicker
 
+    def add_vertix(self, vertix:vertix):
+        self.vertices.enqueue(vertix)
+
+class vertix:
+    def __init__(self):
+        self.buildings = [0]*2
+        self.weight = 0
+    
+    def make_connection(self, build1:Building, build2:Building, time:int):
+        self.buildings = [build1,build2]
+        self.weight = time
+    
+    def get_other(self, building:Building):
+        if self.buildings[0] is building:
+            return self.buildings[1]
+        return self.buildings[0]
+        
 
 class Campus:
 
     def __init__(self):
-
         self.buildings = priorityQ() # building_id -> Building
-        self.pathways   = []    # array of paths
 
-    def find_path(self, source : Building, dest : Building):
-        pass
-        # brute force to find shortest, move only such that the lat,long becomes closer
+    def find_path(self, source, dest):
+        pq = priorityQ()
+        pq.enqueue((source, [source]), 0)  # (current, path), priority = cost
+
+        visited = set()
+
+        while pq.body:
+            priority, (current, path) = pq.dequeue()
+
+            if current in visited:
+                continue
+            visited.add(current)
+
+            if current == dest:
+                return path, priority
+
+            for vert in current.vertices.body:
+                neighbor = vert.get_other(current)
+
+                if neighbor not in visited:
+                    pq.enqueue(
+                        (neighbor, path + [neighbor]),
+                        priority + vert.weight
+                    )
+
+        return None, float("inf")
+
+        # path = []
+        # temp_path = []
+        # temp_weight = [10000000000,-1]
+        # buildings = self.buildings.body.copy()
+        # buildings.remove(source)
+        # curr_building = source
+        # vertices = curr_building.vertices.body.copy()
+        # vertix = vertices[0]
+        # vertices.pop(0)
+        
+        # while True:
+        #     if vertix.buildings.contains(dest):
+        #         if temp_weight[0] > temp_weight[1]:
+        #             path = temp_path
+        #             path.append(vertix)
+        #             temp_weight[0] = temp_weight[1]
+            
+        #     else:
+        #         curr_building = vertix.get_other
+        #         temp_weight[1] += vertix.weight
+        #         temp_path += vertix
+        #         vertices = curr_building.vertices.body.copy()
+        #         vertix = vertices[0]
+        #         vertices.pop(0)
+        #         buildings.remove(curr_building)
+            
+
+                
     
     def add_building(self,item,id):
         self.buildings.enqueue(item,id)
@@ -263,6 +339,45 @@ class History:
     def remove(self):
         return self.nav.pop()
 
-    
 
-    
+if __name__ == "__main__":
+    from math import floor
+    import random
+
+    def names_from_list(list,building):
+        out = []
+        for item in list:
+            out.append(item.get_other(building).building_id)
+        return out
+
+    def weights_from_list(list):
+        out = []
+        for item in list:
+            out.append(item.weight)
+        return out
+    campus = Campus()
+    buildings = [Building]*10
+
+    for i in range(len(buildings)):
+        buildings[i] = Building(f"{i}",f"{i}",(i,i))
+
+    for i in range(len(buildings)):
+        for j in range(len(buildings)):
+            if i == j:
+                break
+
+            if random.random() <= 0.5:
+                temp = vertix()
+                temp.make_connection(buildings[i],buildings[j],floor(random.random()*10))
+                buildings[i].add_vertix(temp)
+                buildings[j].add_vertix(temp)
+    build1 = floor(random.random()*10)  
+    build2 = floor(random.random()*10)
+    path = campus.find_path(buildings[build1],buildings[build2])
+    for building in buildings:
+        print(building.building_id,"connects to",names_from_list(building.vertices.body,building),"with weights",weights_from_list(building.vertices.body))
+
+    print(f"path from {buildings[build1].building_id} to {buildings[build2].building_id}:")
+
+    for building in path[0]:
+        print(building.building_id)
