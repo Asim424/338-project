18d8e8e1223846cda0fcd4416fe39939e1177881
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-12 15:23:06 -0600

    Fix PriorityQueue by making it heap-based

--- src/data_structures.py
+++ src/data_structures.py
@@ -1,23 +1,56 @@
 class PriorityQueue:
     def __init__(self):
-        self.body = []
+        self.data = []
+
+    def print_data(self):
+        for i in range(len(self.data)):
+            print(f"{self.data[i].get_value()}", end=", ")
 
     def enqueue(self, item, priority):
-        inserted = False
-
-        for i in range(len(self.body)):
-            if priority < self.body[i][0]:  # smaller = higher priority
-                self.body.insert(i, [priority, item])
-                inserted = True
-                break
-
-        if not inserted:
-            self.body.append([priority, item])
+        # Insert at the end, and then heapify upward
+        self.data.append((priority, item))
+        self.heapify_up(len(self.data) - 1)
 
     def dequeue(self):
-        if not self.body:
+        # Highest priorty is index zero, so we pop that
+        if len(self.data) == 0:
             return None
-        return self.body.pop(0)  # returns [priority, item]
+        ret = self.data[0]
+
+        # Move the last element to index zero, and heapify downward
+        last = self.data.pop()
+
+        if len(self.data) > 0:
+            self.data[0] = last
+            self.heapify_down(0)
+
+        return ret
+
+    def heapify_up(self, i):
+        if i <= 0:
+            return
+            
+        parent = (i - 1) // 2
+
+        if parent >= 0:
+            if self.data[i][0] < self.data[parent][0]:
+                self.data[i], self.data[parent] = self.data[parent], self.data[i]
+                self.heapify_up(parent)
+
+    def heapify_down(self, i):
+        largest = i
+        left = 2*i + 1
+        right = 2*i + 2
+
+        if left < len(self.data) and self.data[left][0] < self.data[largest][0]:
+            largest = left
+
+        if right < len(self.data) and self.data[right][0] < self.data[largest][0]:
+            largest = right
+
+        if largest != i:
+            self.data[i], self.data[largest] = self.data[largest], self.data[i]
+            self.heapify_down(largest)
 
 class Stack:
     def __init__(self):
