c8eb2748fc3c235f98d1963f6bd95dbdf3c38448
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-15 23:52:45 -0600

    Make lookup demonstrations include insertions

--- src/demonstrations.py
+++ src/demonstrations.py
@@ -13,28 +13,39 @@ def building_lookup_demonstration():
 
     ids = [str(i) for i in range(num_buildings)]
 
-    time_data = {}
+    insert_time_data = {}
+    lookup_time_data = {}
 
     i = 1
     for id in ids:
-        campus.insert_building(Building(id, f"Name{id}", (0, 0)))
+        building = Building(id, f"Name{id}", (0, 0))
+
+        time = timeit(lambda : campus.insert_building(building), number=100) / 100 * 1_000_000_000
+        insert_time_data[i] = time
+
         rand_id = ids[randint(0, i)]
 
         time = timeit(lambda : campus.lookup_building(rand_id), number=100) / 100 * 1_000_000_000
-        time_data[i] = time
+        lookup_time_data[i] = time
 
         i += 1
 
     # Plot
     plt.figure()
-    x = [iter for iter in time_data.keys()];
-    y = [time_data[iter] for iter in x]
+
+    x = [iter for iter in insert_time_data.keys()];
+    y = [insert_time_data[iter] for iter in x]
     plt.plot(x, y)
 
-    plt.title("Building Lookup Time Based on Campus Size")
+    x = [iter for iter in lookup_time_data.keys()];
+    y = [lookup_time_data[iter] for iter in x]
+    plt.plot(x, y)
+
+    plt.title("Operation Time Based on Campus Size")
     plt.xlabel("Number of Buildings")
     plt.ylabel("Time (ns)")
     plt.grid(color="grey", linestyle='-', linewidth=1, alpha=0.5)
+    plt.legend(["Insert", "Lookup"])
 
     plt.show()
 
@@ -71,4 +82,4 @@ def room_lookup_demonstration():
     plt.show()
 
 if __name__ == "__main__":
-    room_lookup_demonstration()
+    building_lookup_demonstration()
